e15ba35714b67bed6f3f3216f6b9ba5786e0911b
Maxime Salomon <maxime@croissant.io>
2019-10-04 08:18:20 +0200

    add items to room class

--- src/room.py
+++ src/room.py
@@ -5,12 +5,17 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items = []
     def __str__(self):
-        return f'room: {self.name}, description: {self.description}'
+        toprint = f'room: {self.name}, description: {self.description}\n'
+        for item in self.items:
+            toprint += f'Items: {item} - {item.description}'
+            return toprint
+
 
     name = str
     description = str
     n_to = str
     s_to = str
     e_to = str
-    w_to = str 
+    w_to = str
